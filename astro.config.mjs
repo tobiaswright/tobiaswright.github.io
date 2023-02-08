58837a9a4119188696d6a8ccd42f458a0bbc3f80
tobias.wright <tobias.wright@outlook.com>
2023-02-08 00:39:06 -0600

    update to config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tobiaswright.github.io'
+    site: 'https://github.com/tobiaswright/tobiaswright.github.io'
 });
